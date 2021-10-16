f845a3c2041d9be24584022ce8aec1f7c6c8ce84
Brendan DeLancey <bdelancey98@gmail.com>
2021-10-16 05:13:32 -0400

    Formating workflow

--- server/generateKeyPair.js
+++ server/generateKeyPair.js
@@ -12,14 +12,14 @@ function genKeyPair() {
     const keyPair = crypto.generateKeyPairSync('rsa', {
         modulusLength: 4096, // bits - standard for RSA keys
         publicKeyEncoding: {
-            type: 'pkcs1', // "Public Key Cryptography Standards 1" 
+            type: 'pkcs1', // "Public Key Cryptography Standards 1"  
             format: 'pem' // Most common formatting choice
         },
         privateKeyEncoding: {
             type: 'pkcs1', // "Public Key Cryptography Standards 1"
             format: 'pem' // Most common formatting choice
         }
-    });
+    }); 
 
     // Create the public key file
     fs.writeFileSync(__dirname + '/id_rsa_pub.pem', keyPair.publicKey); 
