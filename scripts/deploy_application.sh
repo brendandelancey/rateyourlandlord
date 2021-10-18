61184975bec64c34e5e1a0818380ae47d387e5c3
Brendan DeLancey <bdelancey98@gmail.com>
2021-10-17 21:51:54 -0400

    Edited deploy script

--- scripts/deploy_application.sh
+++ scripts/deploy_application.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# give permission to the files inside /secure_docs directory
-
-sudo chmod -R 777 /home/ubuntu/rateyourlandlord
-
 # navigate into current working directory
 
 cd /home/ubuntu/rateyourlandlord
