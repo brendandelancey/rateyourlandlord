78581292b165c9682b0ca2f9bf5263ddc892a8ae
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-14 19:36:15 -0500

    Adding configuration for .env

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const deploy=true;
+const deploy=false;
 require('dotenv').config()
 const deployment = process.env.DATABASE_DEPLOYMENT
 const development = process.env.DATABASE_DEVELOPMENT
