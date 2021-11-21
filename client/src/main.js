bd12932dae1ff36853eb5cae1f200cbf3fa5a1cd
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-20 21:49:49 -0500

    Test deploy frontend

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ Vue.use(IconsPlugin);
 
 // Setting up default vue's http modules for api calls
 Vue.prototype.$http = axios;
-// Load the token from the local Storage
+// Load the token from the localStorage
 const token = localStorage.getItem("token");
 // Is there is any token then we will simply append default axios authorization headers
 if (token) {
