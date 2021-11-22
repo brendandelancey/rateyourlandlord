f1170b6fd8eaed16bd1328d9daee09d2e2c073c9
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-21 20:56:51 -0500

    Test backend deploy

--- client/config/env.js
+++ client/config/env.js
@@ -2,6 +2,7 @@
 
 module.exports ={
   DOMAIN: 'http://www.rateyourstudenthouse.com/api',
+  // DOMAIN: 'http://www.rateyourstudenthouse.com/api',
   LOCAL: 'http://localhost:5000/api',
-  API:'"https://maps.googleapis.com/maps/api/js?key=AIzaSyCe8cGwppUd2cirwhpZFFu_PdvDMgiEUD0&libraries=places"'
+  // API:'"https://maps.googleapis.com/maps/api/js?key=AIzaSyCe8cGwppUd2cirwhpZFFu_PdvDMgiEUD0&libraries=places"'
 }
