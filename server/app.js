36a6f8d9cc00ed1adb32a8909f0b26ae47eb9c6c
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-22 02:47:47 -0500

    Test backend deploy

--- server/app.js
+++ server/app.js
@@ -124,7 +124,7 @@ app.get('/test', (req, res) => {
 /**
  * -------------- SERVER ----------------
  */
-//Probaby should hide the port in a "".env" file using 
+//Probably should hide the port in a "".env" file using 
 //"dotenv" thats what the .env.Port is for
 var deployment=true;
 // const port=process.env.PORT_DEPLOYMENT
