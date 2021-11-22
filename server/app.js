b1cbb59b5d961f815e2e46523029c3d4067172d3
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-21 23:08:55 -0500

    Test backend deploy

--- server/app.js
+++ server/app.js
@@ -187,21 +187,5 @@ var PORT;
 PORT=80
 
 app.listen(PORT, () => {
- console.log(`Server started on port ${PORT}`);
- 	try {
-		//get secretsString:
-		const secretsString = await retrieveSecrets();
-
-		//write to .env file at root level of project:
-		await fs.writeFile(".env", secretsString);
-
-		//configure dotenv package
-		dotenv.config();
-
-		console.log("Server running on port 4000");
-	} catch (error) {
-		//log the error and crash the app
-		console.log("Error in setting environment variables", error);
-		process.exit(-1);
-	}
+    console.log(`Server started on port ${PORT}`);
 })
