c7950b395d39bc23dea6a0764710d92fbfa874f7
Brendan DeLancey <bdelancey98@gmail.com>
2021-11-26 20:01:25 -0500

    Added cros origins fix

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://rateyourstudenthouse.com");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
